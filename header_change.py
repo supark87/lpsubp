1d30c73c47f35f079e435558fa5298e18bf4c26a
supark87 <32421592+supark87@users.noreply.github.com>
2019-11-06 21:32:09 -0500

    Add files via upload

---
+++ header_change.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+import sys
+from Bio import AlignIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+
+cwdir=sys.argv[1]
+
+for file in os.listdir(cwdir):
+     if file.endswith(".fasta"):
+         name=file.split(".")[0]
+         #print(name)
+         with open(file) as original, open(name+"_corrected"+".fasta",'w') as corrected:
+             records=SeqIO.parse(original,'fasta')
+             for record in records:
+                 record.id=name+""+record.id
+                 SeqIO.write(record, corrected, 'fasta')
